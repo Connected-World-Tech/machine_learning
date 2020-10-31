76384a3c9104e8200a47c65d8bfc15b8652b01a5
abhinav-cwt <abhinav@connectedworld.tech>
2020-10-31 13:23:13 +0000

    Debug Logs Test
    Added debug logs for esp32

--- templates/esp32/esp-idf-from-tensorflow-makefile/main/main.cc
+++ templates/esp32/esp-idf-from-tensorflow-makefile/main/main.cc
@@ -12,15 +12,31 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
-
+#include "esp_system.h"
+#include "esp_spi_flash.h"
+#include "esp_log.h"
 #include "main_functions.h"
 
 // This is the default main used on systems that have the standard C entry
 // point. Other devices (for example FreeRTOS or ESP32) that have different
 // requirements for entry code (like an app_main function) should specialize
 // this main.cc file in a target-specific subfolder.
+static const char* TAG = "esp-tflite-template";
 extern "C" void app_main() 
 {
+   /* Print chip information */
+  esp_chip_info_t chip_info;
+  esp_chip_info(&chip_info);
+  ESP_LOGW(TAG, "This is ESP32 chip with %d CPU cores, WiFi%s%s, ",
+    chip_info.cores,
+    (chip_info.features & CHIP_FEATURE_BT) ? "/BT" : "",
+    (chip_info.features & CHIP_FEATURE_BLE) ? "/BLE" : "");
+
+  ESP_LOGW(TAG, "silicon revision %d, ", chip_info.revision);
+
+  ESP_LOGW(TAG, "%dMB %s flash\n", spi_flash_get_chip_size() / (1024 * 1024),
+    (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external");
+
   setup();
   while (true) {
     loop();
